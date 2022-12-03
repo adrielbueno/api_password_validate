114ba0f6f9965071e560ed14d30307100598858b
sergiopallet <sergio.lima@rbmweb.com.br>
2022-12-02 22:10:53 -0300

    Adding phpUnit

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class AverageTest extends TestCase
+{
+    protected $Average;
+
+    public function setUp()
+    {
+        
+    }
+
+    public function testTest()
+    {
+        
+        $this->assertEquals(true, true);
+    }
+
+}
