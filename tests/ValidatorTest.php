26dde51ea12efcfceaa4d27cc70742206372072c
sergiopallet <sergio.lima@rbmweb.com.br>
2022-12-02 22:43:19 -0300

    Adding minUpperCase test

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Modules\PasswordRules\Validation;
 use PHPUnit\Framework\TestCase;
 
 class AverageTest extends TestCase
@@ -10,12 +10,43 @@ class AverageTest extends TestCase
     public function setUp()
     {
         
+      $this->validation = new Validation();
     }
 
-    public function testTest()
+   
+
+    public function testIfItWillReturnTrueAnMinUpperCase()
     {
-        
-        $this->assertEquals(true, true);
+      $password = "ABFSAFASadaDC";
+      $minimum = 3;
+      $result = $this->invokeMethod($this->validation, 'validateMinLowerCase',[$password, $minimum]);
+      $this->assertTrue($result);
     }
+
+    public function testIfItWillGiveAnFalseOnMinUpperCase()
+    {
+      $password = "AB";
+      $minimum = 3;
+      $result = $this->invokeMethod($this->validation, 'validateMinLowerCase',[$password, $minimum]);
+      $this->assertFalse($result);
+    }
+
+    /**
+ * Call protected/private method of a class.
+ *
+ * @param object &$object    Instantiated object that we will run method on.
+ * @param string $methodName Method name to call
+ * @param array  $parameters Array of parameters to pass into method.
+ *
+ * @return mixed Method return.
+ */
+public function invokeMethod(&$object, $methodName, array $parameters = array())
+{
+    $reflection = new \ReflectionClass(get_class($object));
+    $method = $reflection->getMethod($methodName);
+    $method->setAccessible(true);
+
+    return $method->invokeArgs($object, $parameters);
+}
 
 }
